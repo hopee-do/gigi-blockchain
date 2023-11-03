5eaa7690c1790910a109ad8a9ae2fbb59ee5051d
ntdo <ntdo@hopee.com.vn>
2023-11-03 16:27:42 +0700

    chain code

---
+++ asset-transfer-default/chaincode-go/chaincode/smartcontract.go
@@ -0,0 +1,103 @@
+package chaincode
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+)
+
+// SmartContract provides functions for managing an Asset
+type SmartContract struct {
+	contractapi.Contract
+}
+
+// Asset describes basic details of what makes up a simple asset
+// Insert struct field in alphabetic order => to achieve determinism across languages
+// golang keeps the order when marshal to json but doesn't order automatically
+type Asset struct {
+	ID          string `json:"ID"`
+	Action      string `json:"Action"`
+	UserId      int    `json:"UserId"`
+	UserName    string `json:"UserName"`
+	ProductName string `json:"ProductName"`
+	ProductId   int    `json:"ProductId"`
+	Point       int    `json:"Point"`
+}
+
+// InitLedger adds a base set of assets to the ledger
+func (s *SmartContract) InitLedger(ctx contractapi.TransactionContextInterface) error {
+	assets := []Asset{
+		{
+			ID:          "1",
+			Action:      "Buy",
+			UserId:      1,
+			UserName:    "テストユーザー",
+			ProductName: "ELE Coffee",
+			ProductId:   1,
+			Point:       0,
+		},
+	}
+
+	for _, asset := range assets {
+		assetJSON, err := json.Marshal(asset)
+		if err != nil {
+			return err
+		}
+
+		err = ctx.GetStub().PutState(asset.ID, assetJSON)
+		if err != nil {
+			return fmt.Errorf("failed to put to world state. %v", err)
+		}
+	}
+
+	return nil
+}
+
+// CreateAsset issues a new asset to the world state with given details.
+func (s *SmartContract) CreateAsset(ctx contractapi.TransactionContextInterface, id string, action string, userId int, username string, productName string, productId int, point int) error {
+
+	asset := Asset{
+		ID:          id,
+		Action:      action,
+		UserId:      userId,
+		UserName:    username,
+		ProductName: productName,
+		ProductId:   productId,
+		Point:       point,
+	}
+	assetJSON, err := json.Marshal(asset)
+	if err != nil {
+		return err
+	}
+
+	return ctx.GetStub().PutState(id, assetJSON)
+}
+
+// GetAllAssets returns all assets found in world state
+func (s *SmartContract) GetAllAssets(ctx contractapi.TransactionContextInterface) ([]*Asset, error) {
+	// range query with empty string for startKey and endKey does an
+	// open-ended query of all assets in the chaincode namespace.
+	resultsIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, err
+	}
+	defer resultsIterator.Close()
+
+	var assets []*Asset
+	for resultsIterator.HasNext() {
+		queryResponse, err := resultsIterator.Next()
+		if err != nil {
+			return nil, err
+		}
+
+		var asset Asset
+		err = json.Unmarshal(queryResponse.Value, &asset)
+		if err != nil {
+			return nil, err
+		}
+		assets = append(assets, &asset)
+	}
+
+	return assets, nil
+}
