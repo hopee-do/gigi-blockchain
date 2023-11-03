7120f63450f93b29a7799d619eb37bb3983fd4d0
ntdo <ntdo@hopee.com.vn>
2023-11-03 11:23:06 +0700

    Init

---
+++ network/peer1admin.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# look for binaries in local dev environment /build/bin directory and then in local samples /bin directory
+export PATH="${PWD}"/../bin:"$PATH"
+export FABRIC_CFG_PATH="${PWD}"/../config
+
+export FABRIC_LOGGING_SPEC=INFO
+export CORE_PEER_TLS_ENABLED=true
+export CORE_PEER_TLS_ROOTCERT_FILE="${PWD}"/crypto-config/peerOrganizations/blockchain.gigi.com.vn/peers/peer0.blockchain.gigi.com.vn/tls/ca.crt
+export CORE_PEER_ADDRESS=127.0.0.1:7051
+export CORE_PEER_LOCALMSPID=GigiMSP
+export CORE_PEER_MSPCONFIGPATH="${PWD}"/crypto-config/peerOrganizations/blockchain.gigi.com.vn/users/Admin@blockchain.gigi.com.vn/msp
